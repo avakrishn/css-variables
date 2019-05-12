c45d7ef487a40c3b461499ba9b294bc72033f2b7
avakrishn <avakrishn@gmail.com>
2019-05-12 16:26:22 -0700

    add arguments for control inputs and labels to be used in app.js

---
+++ data.mjs
@@ -0,0 +1,25 @@
+'use strict';
+
+export const spacingArgs = [
+    {'name':'spacing'},
+    {'type':'range'},
+    {'value':'10'},
+    {'min':'10'},
+    {'max':'200'},
+    {'data-sizing':'px'}
+];
+
+export const blurArgs = [
+    {'name':'blur'},
+    {'type':'range'},
+    {'value':'10'},
+    {'min':'0'},
+    {'max':'25'},
+    {'data-sizing':'px'}
+];
+
+export const colorArgs = [
+    {'name':'color'},
+    {'type':'color'},
+    {'value':'#ffc600'}
+];
